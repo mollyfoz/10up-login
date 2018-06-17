436493dba46063148e31169b6a809190e59665c3
Molly Foster <mollyrfoster@gmail.com>
2018-06-17 15:04:25 -0600

    hook up custom functions file and css

--- includes/functions/molly-custom.php
+++ includes/functions/molly-custom.php
@@ -1,1 +1,19 @@
+<?php
+namespace TenUpLogin\MollyCustom;
 
+function login_styles() {
+	wp_enqueue_style(
+		'10up_custom_login',
+		TENUP_LOGIN_URL . 'dist/css/style.min.css',
+		[],
+		TENUP_LOGIN_VERSION
+	);
+}
+
+add_action('login_enqueue_scripts', __NAMESPACE__ . '\login_styles');
+
+function custom_login_markup() {
+	
+}
+
+add_action()
