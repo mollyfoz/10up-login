c419d1389eec26ab8c9db63eaecfceeb1d446e98
Molly Foster <mollyrfoster@gmail.com>
2018-06-17 21:15:45 -0600

    change error message and logo title, url

--- includes/functions/molly-custom.php
+++ includes/functions/molly-custom.php
@@ -13,6 +13,29 @@ function override_styles() {
 
 add_action( 'login_enqueue_scripts', __NAMESPACE__ . '\override_styles', 20 );
 
-function custom_login_markup() {
-
+//add custom fonts to login form
+function custom_add_google_fonts() {
+	wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css?family=EB+Garamond:400i|Raleway:400,600', false );
 }
+
+add_action( 'wp_enqueue_scripts', 'custom_add_google_fonts' );
+
+//update logo link and URL to direct to 10up with filters
+function logo_url() {
+	return get_bloginfo( 'https://10up.com/' );
+}
+
+add_filter( 'login_headerurl', 'logo_url' );
+
+function logo_url_title() {
+	return '10up.com';
+}
+
+add_filter( 'login_headertitle', 'logo_url_title' );
+
+//edit login error message
+function login_error_override() {
+    return 'Incorrect email or password.';
+}
+
+add_filter('login_errors', 'login_error_override');
